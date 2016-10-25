2f2e254385c4ca5565aea7db9f11a30b77c34d59
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2016-10-25 19:45:24 +0000

    DM's Create file from wk4

--- create-env.sh
+++ create-env.sh
@@ -5,8 +5,7 @@ aws ec2 run-instances --image-id $1 --key-name $2 --client-token dm41zk21 --secu
  aws elb create-load-balancer --load-balancer-name dmcwk4 --listeners 
 Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --subnets subnet-cb246bbd
 
- aws elb create-load-balancer --load-balancer-name dmartihawk --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --availability-zone us-west-2b
-dmartihawk-1323102667.us-west-2.elb.amazonaws.com
+ aws elb create-load-balancer --load-balancer-name dmartihawk --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --availability-zone us-west-2b dmartihawk-1323102667.us-west-2.elb.amazonaws.com
 
 aws elb register-instances-with-load-balancer --load-balancer-name dmartihawk --instances i-0f9678e838323cbd5 i-06d5720cd2d3ad39a i-0da5b00478f82a730
 
